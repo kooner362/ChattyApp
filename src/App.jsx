33f0d9607cf2e325530ba18fe698b60d93bc7ece
Gurp <kooner@gmail.com>
2019-04-09 18:38:27 +0000

    Completed Data Flow task

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
+
+const data = {
+  currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+  messages: [
+    {
+      id: 1,
+      username: "Bob",
+      content: "Has anyone seen my marbles?",
+    },
+    {
+      id: 2,
+      username: "Anonymous",
+      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+    }
+  ]
+}
 
 function NavBar() {
   return (
@@ -10,13 +26,25 @@ function NavBar() {
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {loading: true};
+  }
+  componentDidMount() {
+    this.setState({loading: false, messages: data.messages, currentUser: data.currentUser})
+  }
+
   render() {
+    let currentUser = undefined;
+    if (!this.state.loading) {
+      currentUser = this.state.currentUser.name;
+    }
     return (
-      <div>
+      <Fragment>
         {NavBar()}
-        <MessageList />
-        <ChatBar />
-      </div>
+        <MessageList messages={this.state.messages}/> 
+        <ChatBar currentUser= {currentUser}/>
+      </Fragment>
     );
   }
 }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+  constructor(props) {
+    super(props)
+  }
   render() {
     return (
         <footer className="chatbar">
-            <input className="chatbar-username" placeholder="Your Name (Optional)" />
+            <input type="text" className="chatbar-username" value={this.props.currentUser} placeholder="Your Name (Optional)" />
             <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
         </footer>
     );
