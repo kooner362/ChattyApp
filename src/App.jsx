dc137cc09cd4eb7efbcbfa7057067d8568fb7e2a
Gurp <kooner@gmail.com>
2019-04-09 00:22:44 +0000

    Moved components into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <a href="/" className="navbar-brand">Chatty</a>
+    </nav>);
+}
 
 class App extends Component {
   render() {
     return (
-      <nav className="navbar">
-        <a href="/" className="navbar-brand">Chatty</a>
-      </nav>
+      <div>
+        {NavBar()}
+        <MessageList />
+        <ChatBar />
+      </div>
     );
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -7,8 +7,5 @@ require('../styles/application.scss');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import MessageList from './MessageList.jsx';
-import ChatBar from './ChatBar.jsx';
 
-
-ReactDOM.render(<div><App /> <MessageList/> <ChatBar /></div>, document.getElementById('react-root'));
+ReactDOM.render(<App />, document.getElementById('react-root'));
