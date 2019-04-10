b57238f81a421003cc12070cecf9fb5a94317fc7
Gurp <kooner@gmail.com>
2019-04-10 00:13:17 +0000

    Added username

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,6 @@ import React, {Component, Fragment} from 'react';
 import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
 
-const data = {
-  currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-  messages: [
-    {
-      id: 1,
-      username: "Bob",
-      content: "Has anyone seen my marbles?",
-    },
-    {
-      id: 2,
-      username: "Anonymous",
-      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-    }
-  ]
-}
-
 function NavBar() {
   return (
     <nav className="navbar">
@@ -28,37 +12,36 @@ function NavBar() {
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {messages: data.messages, currentUser: data.currentUser};
+    this.state = {messages: [], currentUser: 'Bob'};
     this.addMessage = this.addMessage.bind(this);
+    this.updateUser = this.updateUser.bind(this);
     this.ws = new WebSocket("ws://localhost:3001");
   }
 
   componentDidMount() {
     console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-    }, 3000);
+    this.ws.onmessage = (event) => {
+      let newMessage = JSON.parse(event.data);
+      const newMessages = this.state.messages.concat(newMessage);
+      this.setState({messages: newMessages})
+    }
   }
 
   addMessage(message) {
     this.ws.send(JSON.stringify(message));
-    const newMessages = this.state.messages.concat(message);
-    this.setState({messages: newMessages});
+  }
+
+  updateUser(name) {
+    console.log(name);
+    this.setState({currentUser: name});
   }
 
   render() {
-    const currentUser = this.state.currentUser.name;
     return (
       <Fragment>
         {NavBar()}
         <MessageList messages={this.state.messages}/> 
-        <ChatBar addMessage={this.addMessage} currentUser= {currentUser}/>
+        <ChatBar updateUser={this.updateUser} addMessage={this.addMessage} currentUser= {this.state.currentUser}/>
       </Fragment>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,20 +1,25 @@
 import React, {Component} from 'react';
-import { generateRandomId } from "./utils";
 
 class ChatBar extends Component {
   render() {
     const handleKeyDown = (event) => {
       if (event.key === 'Enter') {
         let text = event.target.value;
-        let message = {id: generateRandomId(), username: this.props.currentUser, content: text};
+        let message = {username: this.props.currentUser, content: text};
         event.target.value = '';
         this.props.addMessage(message);
-        
       }
     }
+
+    const handleUser = (event) => {
+      event.preventDefault();
+      let text = event.target.value;
+      this.props.updateUser(text);
+    };
+
     return (
       <footer className="chatbar">
-        <input type="text" className="chatbar-username" defaultValue={this.props.currentUser} placeholder="Your Name (Optional)" />
+        <input name="user" onChange={handleUser} type="text" className="chatbar-username" value={this.props.currentUser} placeholder="Your Name (Optional)" />
         <input name="message" className="chatbar-message" onKeyDown={handleKeyDown} placeholder="Type a message and hit ENTER" />
       </footer>
     );
